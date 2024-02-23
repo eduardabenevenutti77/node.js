9fff87e186c097bd0ee6d17f864692aea22f3549
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-02-23 19:17:03 -0300

    update

--- 20_02_24/ativ5.js
+++ 20_02_24/ativ5.js
@@ -1,1 +1,45 @@
 // 5) Crie um programa que calcule bhaskara
+
+const readLine = require('readline')
+
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let ladoA = [];
+let ladoB = [];
+let ladoC = [];
+
+function inserindoValoresLados(i) {
+    rl.question(`Informe o valor do lado A: `, (a) => {
+        ladoA.push(Number(a));
+        if(ladoA.length<1){
+            inserindoValoresLados(ladoA.length)
+        } else {
+            rl.question(`Informe o valor do lado B: `, (b) => {
+                ladoB.push(Number(b));
+                if(ladoB.length<1){
+                    inserindoValoresLados(ladoB.length);
+                } else {
+                    rl.question(`Informe o valor do lado C: `, (c) => {
+                        ladoC.push(Number(c));
+                        if(ladoC.length<1){
+                            inserindoValoresLados(ladoC.length);
+                        } else {
+                            calculoBhaskara();
+                            rl.close();
+                        }
+                    })
+                }
+            })
+        }
+    })
+}
+//  (-b±√(b²-4ac))/(2a)
+function calculoBhaskara() {
+    let calculo1 = (ladoB * 2 - 4 * ladoA * ladoC);
+    let calculo2 = ()
+}
+
+inserindoValoresLados(0);
