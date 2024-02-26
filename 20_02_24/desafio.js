6ad5103c28441adf0c4ab65ba76fba23bdea2d6a
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2024-02-26 17:00:01 -0300

    update

--- 20_02_24/desafio.js
+++ 20_02_24/desafio.js
@@ -1,1 +1,30 @@
 // Crie um programa que teste se um número é primo
+
+const readLine = require('readline');
+
+const rl = readLine.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let numero = [];
+
+function inserindoValores(i) {
+    rl.question('Informe o valor: ', (nm) => {
+        numero.push(Number(nm));
+        if (numero < 1){
+            inserindoValores(numero.length);
+        } else {
+            verificaNumero();
+            rl.close();
+        }
+    })
+}
+
+function verificaNumero() {
+    if (numero[0] / numero[0] == 1 ) {
+        
+    }
+}
+
+inserindoValores(0);
